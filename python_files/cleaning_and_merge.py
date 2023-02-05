aba36dc3a669d1453a68929857d65afe2e68ddf1
SergiColl5 <sergicoll1995@gmail.com>
2023-02-05 17:03:19 +0100

    Commit at:

--- python_files/cleaning_and_merge.py
+++ python_files/cleaning_and_merge.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from functions import cleaning_functions as clean
+import my_functions as mf
 
 # Reading the two files i need.
 migration = pd.read_excel('../data/downloaded_excel.xlsx',sheet_name='1.4')
 income = pd.read_excel('../data/income_industry.xlsx',sheet_name='Table 2.1')
 
 # Main cleaning for both
-migration=clean.BasicCleanig(migration,5,5)
-income=clean.BasicCleanig(income,5,5)
+migration=mf.BasicCleanig(migration,5,5)
+income=mf.BasicCleanig(income,5,5)
 
 
